d18dc6ff3f5e92f55ca8a67c70fac71b8f0b068d
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-29 13:08:52 +1000

    chore: add getUser function to get user for each user_id from each place

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,43 +1,60 @@
 $(document).ready(function () {
-  console.log("READY");
-
-  function placesSearch() {
-    $.post("http://0.0.0.0:5001/api/v1/places_search/",
-      contentType = 'application/json',
-      data = JSON.stringify({}),
-      function (data) {
-        if (data.status === "OK") {
-          data.forEach(place => {
-            const article = $('<article>');
-            
-            const titleBox = $('<div>').addClass('title_box');
-            const placeName = $('<h2>').text(place.name);
-            const priceByNight = $('<div>').addClass('price_by_night').text(`$${place.price_by_night}`);
-            titleBox.append(placeName, priceByNight);
-    
-            const information = $('<div>').addClass('information');
-            const maxGuest = $('<div>').addClass('max_guest').text(`${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}`);
-            const numberRooms = $('<div>').addClass('number_rooms').text(`${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}`);
-            const numberBathrooms = $('<div>').addClass('number_bathrooms').text(`${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}`);
-            information.append(maxGuest, numberRooms, numberBathrooms);
-
-            const user = $('<div>').addClass('user');
-            const owner = $('<b>').text('Owner:');
-            user.append(owner).text(`${ place.user.first_name } ${ place.user.last_name }`)
-    
-            const description = $('<div>').addClass('description').html(place.description);
-    
-            article.append(titleBox, information, user, description);
-    
-            $('section.places').append(article);
-          });
-        } else {
-        }
-      }).fail(function () {
-        $("#api_status").removeClass("available");
-      });
+  async function getUser(id) {
+    $.ajax({
+      url: `http://0.0.0.0:5001/api/v1/users/${id}`,
+      type: "GET",
+      contentType: "application/json",
+      data: JSON.stringify({}),
+      success: function (data) {
+        return data
+      },
+      error: function () {
+        throw new Error("couldn't get user data")
+      }
+    });
   }
 
+  function placesSearch() {
+    $.ajax({
+      url: "http://0.0.0.0:5001/api/v1/places_search/",
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify({}),
+      success: function (data) {
+        $('section.places').empty();
+        data.forEach(place => {
+          const article = $('<article>');
+          getUser(place.user_id)
+          const titleBox = $('<div>').addClass('title_box');
+          const placeName = $('<h2>').text(place.name);
+          const priceByNight = $('<div>').addClass('price_by_night').text(`$${place.price_by_night}`);
+          titleBox.append(placeName, priceByNight);
+  
+          const information = $('<div>').addClass('information');
+          const maxGuest = $('<div>').addClass('max_guest').text(`${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}`);
+          const numberRooms = $('<div>').addClass('number_rooms').text(`${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}`);
+          const numberBathrooms = $('<div>').addClass('number_bathrooms').text(`${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}`);
+          information.append(maxGuest, numberRooms, numberBathrooms);
+  
+          const user = $('<div>').addClass('user');
+          const owner = $('<b>').text('Owner:');
+          const userData = getUser(place.user_id) // This is bad approach. Place data should includes user data
+          user.append(owner).append(` ${userData.first_name} ${userData.last_name}`);
+  
+          const description = $('<div>').addClass('description').html(place.description);
+  
+          article.append(titleBox, information, user, description);
+  
+          $('section.places').append(article);
+        });
+      },
+      error: function () {
+        $("#api_status").removeClass("available");
+      }
+    });
+  }
+
+  placesSearch();
   // Function to check API status
   function checkApiStatus() {
     $.get("http://localhost:5001/api/v1/status/", function (data) {
